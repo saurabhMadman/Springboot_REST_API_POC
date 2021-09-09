311f9f9768d4a06ea1ca8b1c5c6b6163e697d9df
Saurabhv <Saurabhv@nextgen.com>
2021-09-09 16:27:54 +0530

    Application Controller,Service and Dao

---
+++ inventoryManagementRestService/inventoryManagementRestService/src/main/java/com/example/inventoryManagementRestService/InventoryManagementRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryManagementRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementRestServiceApplication.class, args);
+	}
+
+}
---
+++ inventoryManagementRestService/inventoryManagementRestService/src/test/java/com/example/inventoryManagementRestService/InventoryManagementRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryManagementRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
